b1d388492b5e44a0b2660e623a76db6f46eab7bf
Daniele Bedini <108469620+danielebedini@users.noreply.github.com>
2024-03-12 12:08:21 +0100

    added new plots

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Description: This file is used to test the spectrogram plotting function
 # Usage: python main.py
 
-from utils import plot_spectrogram, plot_phase_spectrogram, plot_magnitude_and_phase_soectrogram, detect_instrument
+from utils import plot_magnitude_and_phase_spectrogram, plot_chromagram, plot_mfcc, plot_waveform
 
 # Load the audio file
-path_guitar = 'all-samples/guitar/guitar_A2_very-long_forte_normal.mp3'
+path_guitar = 'all-samples/guitar/guitar_F4_very-long_forte_normal.mp3'
+path_guitar2 = 'all-samples/guitar/guitar_G3_very-long_piano_normal.mp3'
 path_flute = 'all-samples/flute/flute_A4_1_forte_normal.mp3'
 path_mandolin = 'all-samples/mandolin/mandolin_A3_very-long_forte_tremolo.mp3'
 path_saxophone = 'all-samples/saxophone/saxophone_A3_1_forte_normal.mp3'
@@ -21,14 +22,28 @@ path_saxophone = 'all-samples/saxophone/saxophone_A3_1_forte_normal.mp3'
 # plot_phase_spectrogram(filepath_3)
 # plot_phase_spectrogram(filepath_4)
 
-# Plot the magnitude and phase spectrogram
-plot_magnitude_and_phase_soectrogram(path_guitar)
-plot_magnitude_and_phase_soectrogram(path_flute)
-plot_magnitude_and_phase_soectrogram(path_mandolin)
-plot_magnitude_and_phase_soectrogram(path_saxophone)
+# Plot the waveform
+plot_waveform(path_guitar)
+#plot_waveform(path_guitar2)
+plot_waveform(path_flute)
+#plot_waveform(path_mandolin)
+#plot_waveform(path_saxophone)
 
-# Detect the instrument from the audio file
-detect_instrument(path_guitar)
-detect_instrument(path_flute)
-detect_instrument(path_mandolin)
-detect_instrument(path_saxophone)
+# Plot the magnitude and phase spectrogram
+plot_magnitude_and_phase_spectrogram(path_guitar)
+#plot_magnitude_and_phase_spectrogram(path_guitar2)
+plot_magnitude_and_phase_spectrogram(path_flute)
+#plot_magnitude_and_phase_spectrogram(path_mandolin)
+#plot_magnitude_and_phase_spectrogram(path_saxophone)
+
+plot_chromagram(path_guitar)
+#plot_chromagram(path_guitar2)
+plot_chromagram(path_flute)
+#plot_chromagram(path_mandolin)
+#plot_chromagram(path_saxophone)
+
+plot_mfcc(path_guitar)
+#plot_mfcc(path_guitar2)
+plot_mfcc(path_flute)
+#plot_mfcc(path_mandolin)
+#plot_mfcc(path_saxophone)
--- utils.py
+++ utils.py
@@ -2,6 +2,16 @@ import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# Plot the waveform
+def plot_waveform(path):
+    y, sr = librosa.load(path, sr=None)
+    plt.figure(figsize=(8, 3))
+    librosa.display.waveshow(y, sr=sr)
+    plt.title('Waveform')
+    plt.show()
+
+# Plot the spectrogram
 def plot_spectrogram(path):
 
     # Load the audio file
@@ -11,7 +21,7 @@ def plot_spectrogram(path):
     S_full, phase = librosa.magphase(librosa.stft(y))
 
     # Plot the magnitude spectrogram
-    plt.figure(figsize=(12, 4))
+    plt.figure(figsize=(8, 3))
     librosa.display.specshow(librosa.amplitude_to_db(S_full, ref=np.max), y_axis='log', x_axis='time', sr=sr)
     plt.colorbar(format='%+2.0f dB')
     plt.title('Spectrogram')
@@ -19,11 +29,10 @@ def plot_spectrogram(path):
     plt.show()
 
 # Plot the phase spectrogram
-
 def plot_phase_spectrogram(path):
     y, sr = librosa.load(path, sr=None)
     S_full, phase = librosa.magphase(librosa.stft(y))
-    plt.figure(figsize=(12, 4))
+    plt.figure(figsize=(8, 3))
     librosa.display.specshow(phase, y_axis='log', x_axis='time', sr=sr)
     plt.colorbar()
     plt.title('Phase')
@@ -31,25 +40,41 @@ def plot_phase_spectrogram(path):
     plt.show()
 
 # Plot the magnitude and phase spectrogram
-def plot_magnitude_and_phase_soectrogram(path):
+def plot_magnitude_and_phase_spectrogram(path):
     
     y, sr = librosa.load(path, sr=None)
     S_full, phase = librosa.magphase(librosa.stft(y))
-    plt.figure(figsize=(12, 4))
+    plt.figure(figsize=(8, 3))
     plt.subplot(1, 2, 1)
     librosa.display.specshow(librosa.amplitude_to_db(S_full, ref=np.max), y_axis='log', x_axis='time', sr=sr)
     plt.colorbar(format='%+2.0f dB')
     plt.title('Magnitude spectrogram')
 
     plt.subplot(1, 2, 2)
-    librosa.display.specshow(phase, y_axis='log', x_axis='time', sr=sr)
+    librosa.display.specshow(S_full, y_axis='log', x_axis='time', sr=sr)
     plt.colorbar()
-    plt.title('Phase spectrogram')
+    plt.title('STFT spectrogram')
 
     plt.show()
 
-# Detect the instrument from the audio file
-def detect_instrument(path):
+# plot the chromagram
+def plot_chromagram(path):
     y, sr = librosa.load(path, sr=None)
-    S_full, phase = librosa.magphase(librosa.stft(y))
-    return S_full, phase
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+    plt.figure(figsize=(8, 3))
+    librosa.display.specshow(chroma, y_axis='chroma', x_axis='time', sr=sr, cmap='coolwarm')
+    plt.colorbar()
+    plt.title('Chromagram')
+    plt.show()
+
+# plot Mel-Frequency Cepstral Coefficients 
+    
+def plot_mfcc(path):
+    y, sr = librosa.load(path, sr=None)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, dct_type=2)
+    plt.figure(figsize=(8, 3))
+    librosa.display.specshow(mfcc, x_axis='time', sr=sr)
+    plt.colorbar()
+    plt.title('MFCC')
+    plt.show()
+
